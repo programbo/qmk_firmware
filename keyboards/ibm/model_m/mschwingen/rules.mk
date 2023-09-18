9bff9fbade41ef1ffc35a6996eda40e5592541b7
zvecr <git@zvecr.com>
2023-09-18 21:01:10 +0100

    Implement multiple logging backends

--- keyboards/ibm/model_m/mschwingen/rules.mk
+++ keyboards/ibm/model_m/mschwingen/rules.mk
@@ -19,8 +19,7 @@ DYNAMIC_MACRO_ENABLE = yes
 UART_DEBUG = no
 
 SRC += matrix.c
-QUANTUM_LIB_SRC += uart.c \
-                   spi_master.c
+QUANTUM_LIB_SRC += spi_master.c
 
 OPT_DEFS += -DSLEEP_LED_ENABLE # we need our own sleep callbacks to turn of WS2812 LEDs
 
